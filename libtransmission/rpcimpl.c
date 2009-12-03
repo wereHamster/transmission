wereHamster/transmission

commit e643d79fa107e56ff54b972d5e44b7f47c56681d

(trunk) #2096 "Magnet Links - Extension for Peers to Send Metadata Files (BEP #9)" -- publish tr_stat.metadataPecentComplete in the RPC so that kjg can use it in the web client
git-svn-id: svn://svn.m0k.org/Transmission/trunk@9655 f4695dd4-2c0a-0410-b89c-da849a56a58e

libtransmission/rpcimpl.c
@@ -500,6 +500,8 @@ addField( const tr_torrent * tor, tr_benc * d, const char * key )
         tr_bencDictAddInt( d, key, st->manualAnnounceTime );
     else if( tr_streq( key, keylen, "maxConnectedPeers" ) )
         tr_bencDictAddInt( d, key,  tr_torrentGetPeerLimit( tor ) );
+    else if( tr_streq( key, keylen, "metadataPecentComplete" ) )
+        tr_bencDictAddReal( d, key, st->metadataPercentComplete );
     else if( tr_streq( key, keylen, "name" ) )
         tr_bencDictAddStr( d, key, inf->name );
     else if( tr_streq( key, keylen, "percentDone" ) )

commit 178edb4df31cebb07165fc24193ac7dee37220ef

(trunk) #2096 "magnet links" -- fix RPC typo introduced in r9655 and reported by KyleK
git-svn-id: svn://svn.m0k.org/Transmission/trunk@9660 f4695dd4-2c0a-0410-b89c-da849a56a58e

libtransmission/rpcimpl.c
@@ -500,7 +500,7 @@ addField( const tr_torrent * tor, tr_benc * d, const char * key )
         tr_bencDictAddInt( d, key, st->manualAnnounceTime );
     else if( tr_streq( key, keylen, "maxConnectedPeers" ) )
         tr_bencDictAddInt( d, key,  tr_torrentGetPeerLimit( tor ) );
-    else if( tr_streq( key, keylen, "metadataPecentComplete" ) )
+    else if( tr_streq( key, keylen, "metadataPercentComplete" ) )
         tr_bencDictAddReal( d, key, st->metadataPercentComplete );
     else if( tr_streq( key, keylen, "name" ) )
         tr_bencDictAddStr( d, key, inf->name );